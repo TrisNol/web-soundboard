85172fc13a4ebd5e6c687904985544b8d3d946ad
TrisNol <tristan.nolde@yahoo.de>
2024-08-17 09:01:04 +0000

    checkpoint(server): serve mp3 through HTTP API

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { SoundController } from './sound/sound.controller';
+
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, SoundController],
   providers: [AppService],
 })
 export class AppModule {}
