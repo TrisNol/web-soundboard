40e738ea69c508aea53e7ff8eddd8b52ab26bb19
TrisNol <tristan.nolde@yahoo.de>
2024-08-17 09:01:34 +0000

    Revert "checkpoint(server): serve mp3 through HTTP API"
    This reverts commit 85172fc13a4ebd5e6c687904985544b8d3d946ad.

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -3,11 +3,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { SoundController } from './sound/sound.controller';
-
 @Module({
   imports: [],
-  controllers: [AppController, SoundController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,62 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { SoundboardService } from "./services/soundboard.service";
+import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HttpClientModule],
-  providers: [SoundboardService],
+  imports: [NxWelcomeComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'frontend';
-
-  private audioContext: AudioContext;
-  private microphoneStream: MediaStream | undefined = undefined;
-
-  constructor(private soundboardService: SoundboardService) {
-    this.audioContext = new (window.AudioContext)();
-  }
-
-  ngOnInit() {
-    this.requestMicrophoneAccess();
-  }
-
-  requestMicrophoneAccess() {
-    navigator.mediaDevices.getUserMedia({ audio: true })
-      .then((stream: MediaStream) => {
-        this.microphoneStream = stream;
-        console.log('Microphone access granted');
-      })
-      .catch((err: Error) => {
-        console.error('Microphone access denied', err);
-      });
-  }
-
-  playSound() {
-    this.soundboardService.fetchAudio().subscribe((audioBlob: Blob) => {
-      const audioUrl = URL.createObjectURL(audioBlob);
-      this.playAudio(audioUrl);
-    });
-  }
-
-  playAudio(url: string) {
-    const audioElement = new Audio(url);
-    const sourceNode = this.audioContext.createMediaElementSource(audioElement);
-    const destinationNode = this.audioContext.createMediaStreamDestination();
-
-    sourceNode.connect(destinationNode);
-    sourceNode.connect(this.audioContext.destination);  // To play through speakers
-    if (this.microphoneStream)
-      this.microphoneStream.getTracks().forEach(track => destinationNode.stream.addTrack(track));
-
-    audioElement.play().then(() => {
-      console.log('Playback started successfully');
-    }).catch(error => {
-      console.error('Playback failed:', error);
-    });
-  }
 }
